d364c8b868454ee01435e0110c428756a154e827
s1511409 <s1511409@u.tsukuba.ac.jp>
2017-10-19 13:25:32 +0900

    Initialization

---
+++ app/src/main/java/com/example/masa/mysize/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.masa.mysize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String NECK = "NECK";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
